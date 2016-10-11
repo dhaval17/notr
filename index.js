53197047c9b9850634fbf002b6904e36d85b19b2
Dhaval Chauhan <dhavalchauhan171994@gmail.com>
2016-10-11 17:24:41 +0530

    Browser Window changes

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+	  width: 800, 
+	  height: 600,
+	  maximizable: false,
+	  fullscreenable: false,
+	  frame: true,
+	  resizable: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/content/index.html`)
